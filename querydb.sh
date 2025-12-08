3823068347af2a170eb505be4e55320711ad2df3
Ezekile <rajezekielangel@gmail.com>
2025-12-09 00:37:52 +0530

    changes from termux query

--- querydb.sh
+++ querydb.sh
@@ -5,7 +5,7 @@ TODAY=$(date +"%Y-%m-%d")
 
 DB="${TODAY}.db"
 
-QUERY="SELECT * FROM stocks WHERE p_e < 30 AND week_change_per < -5;"
+QUERY="SELECT * FROM stocks WHERE p_e > 20 and p_e < 30 AND week_change_per < -5;"
 
 echo "Querying DB: $DB"
 echo "Running: $QUERY"
